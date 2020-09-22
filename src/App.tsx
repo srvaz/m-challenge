c07225655de23d4fce9112ab51d8ef7a140f9932
srvaz <jonwify@gmail.com>
2020-09-21 23:10:22 -0300

    :sparkles: Add MAppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { BUTTON_VARIANTS, MButton } from './components/MButton';
+import './App.scss';
+
 import { MText, TEXT_VARIANT } from './components/MText';
 
+import { MAppBar } from './components/MAppBar';
 import React from 'react';
-import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
+      <MAppBar
+        menuItems={[
+          { label: 'Home', path: '#' },
+          { label: 'Agendar', path: '#' },
+          { label: 'Agendamentos', path: '#' },
+        ]}
+        className="App__app-bar"
+      />
       <MText
         variant={TEXT_VARIANT.TITLE}
         tag="h1"
       >
         Hello, World! <span role="img" aria-label="emoji party">🎉</span>
       </MText>
-      <MButton icon={faCode} variant={BUTTON_VARIANTS.PRIMARY} />
     </div>
   );
 }
